c376cfdf4b6370d7b3657aaeaf1f3c4d809416d1
CodeLover82 <wiz9243@gmail.com>
2022-02-24 17:55:30 +0900

    test

---
+++ src/main/java/backend/api/controller/PlanController.java
@@ -0,0 +1,12 @@
+package backend.api.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/join")
+public class PlanController {
+
+}
